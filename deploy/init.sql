ccdbbcee7403d39c3c76463ac0640d7549f074d6
dylan.li <dylan.li@sap.com>
2017-03-24 15:10:11 +0800

    Add mysql init script

---
+++ deploy/init.sql
@@ -0,0 +1,4 @@
+USE mysql;
+  FLUSH PRIVILEGES;
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+  UPDATE user SET password=PASSWORD("MY_PASSWORD") WHERE user='root';
--- deploy/run.sh
+++ deploy/run.sh
@@ -3,10 +3,21 @@
 if [ -z $MYSQL_ROOT_PASSWORD ]; then
       exit 1
   fi
- sed -i "s/MY_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /opt/cnxs.sql
+ sed -i "s/MY_PASSWORD/$MYSQL_ROOT_PASSWORD/g" /opt/init.sql
   mysql_install_db --user mysql > /dev/null
 
-  mysqld --bootstrap --verbose=0 < /opt/cnxs.sql
+ mysqld --bootstrap --verbose=0 < /opt/init.sql
 
  nohup  mysqld > /opt/db.log 2>&1 &
+locktom="running"
+
+while [ "$(echo $locktom)" != "ok" ]; do
+  sleep 1
+  mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD processlist
+  if [ $? -eq 0 ]
+        then
+            locktom="ok"
+            mysql -uroot -p$MYSQL_ROOT_PASSWORD < /opt/cnxs.sql
+  fi
+done
  /usr/local/tomcat/bin/catalina.sh run
