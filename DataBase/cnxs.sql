b379cccbab237263992ffd56ba266240b7ac0d7e
lightPang <light.pang@gmail.com>
2017-03-25 23:32:11 +0800

    update db sql

--- DataBase/cnxs.sql
+++ DataBase/cnxs.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for osx10.12 (x86_64)
 --
 -- Host: localhost    Database: cnxs
 -- ------------------------------------------------------
@@ -21,6 +21,33 @@ DROP DATABASE IF EXISTS `cnxs`;
 CREATE DATABASE `cnxs`;
 
 USE `cnxs`;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `summary` text NOT NULL,
+  `content` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` VALUES (1,'中国内地首条国产无人驾驶地铁线路年内开通','test','新华社北京３月２５日电（记者王迪、孔祥鑫）记者２４日从北京市住建委获悉，今年北京地铁将建成开通中国内地首条全国产化无人驾驶线路——燕房线。该线路采用中国自主研发的无人驾驶地铁车辆和技术，也是北京首条全自动驾驶线路。','据介绍，燕房线位于北京西南部，全长约１６．６公里，共设有９座车站，预计列车最高运行速度可达１００千米／小时，共４辆编组，最大载客量为１２６２人。作为北京首条能够真正实现无人驾驶的线路，燕房线的地铁列车达到了世界最高自动化等级标准，从唤醒到出库、发车、行驶、停车、开关门、回库、休眠、洗车等均由控制中心自动控制，降低了人为操作失误可能带来的安全风险。');
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -56,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-23 17:23:38
+-- Dump completed on 2017-03-25 23:28:31
