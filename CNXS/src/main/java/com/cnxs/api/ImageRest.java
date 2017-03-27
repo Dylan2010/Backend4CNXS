8cacbade1e337a5a5fbbc2248427b30ac2251345
lightPang <light.pang@gmail.com>
2017-03-27 14:52:36 +0800

    add image upload support.

---
+++ CNXS/src/main/java/com/cnxs/api/ImageRest.java
@@ -0,0 +1,59 @@
+package com.cnxs.api;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(value = { "Image/v1" })
+public class ImageRest {
+    
+    private static String UPLOAD_FILE_PATH = System.getenv("CNXS_FILE_UPLOAD_PATH");
+    
+    private static List<String> VALID_FILE_TYPES = new ArrayList<String>();
+    
+    static{
+        VALID_FILE_TYPES.add("JPEG");
+        VALID_FILE_TYPES.add("JPG");
+        VALID_FILE_TYPES.add("PNG");
+    }
+    
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        if(file.isEmpty()) {
+            return new ResponseEntity<String>("File is empty!", HttpStatus.BAD_REQUEST);
+        }
+        
+        try {
+            
+            String fileType = FilenameUtils.getExtension(file.getOriginalFilename());
+            if(VALID_FILE_TYPES.contains(fileType.toUpperCase())) {
+                
+                byte[] bytes = file.getBytes();
+                Date date = new Date();
+                Path path = Paths.get(UPLOAD_FILE_PATH + date.getTime() + "."+fileType);
+                Files.write(path, bytes);
+                return new ResponseEntity<String>("Success!", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<String>("File type invalid!", HttpStatus.BAD_REQUEST);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<String>("Error Occor when upload file!", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
--- CNXS/src/main/java/com/cnxs/api/SimpleRest.java
+++ CNXS/src/main/java/com/cnxs/api/SimpleRest.java
@@ -1,31 +1,25 @@
 package com.cnxs.api;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
 @RequestMapping(value = { "Simple/v1" })
 public class SimpleRest {
-   
+    
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<String> getSample() {
         return new ResponseEntity<String>(System.getenv("MYSQL_ROOT_PASSWORD"), HttpStatus.OK);
@@ -45,23 +39,4 @@ public class SimpleRest {
         return new ResponseEntity<byte[]>(data,headers, HttpStatus.CREATED);
     }
     
-    @PostMapping("/upload")
-    public ResponseEntity<Boolean> uploadFile(@RequestParam("file") MultipartFile file) {
-    	if(file.isEmpty()) {
-    		return new ResponseEntity<Boolean>(false, HttpStatus.BAD_REQUEST);
-    	}
-    	
-    	try {
-    		
-            byte[] bytes = file.getBytes();
-//            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-//            Files.write(path, bytes);
-            
-            return new ResponseEntity<Boolean>(true, HttpStatus.BAD_REQUEST);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ResponseEntity<Boolean>(false, HttpStatus.BAD_REQUEST);
-        }
-    }
 }
