abd9249c0ebac215877141497c722d57dab8401c
lightPang <light.pang@gmail.com>
2017-03-27 10:59:45 +0800

    cannot update creation time.

--- CNXS/src/main/java/com/cnxs/bo/BusinessObjectBase.java
+++ CNXS/src/main/java/com/cnxs/bo/BusinessObjectBase.java
@@ -8,7 +8,7 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public abstract class BusinessObjectBase {
     
-    @Column(name="creationtime")
+    @Column(name="creationtime", updatable=false)
     private Date creationTime;
     
     public Date getCreationTime() {
