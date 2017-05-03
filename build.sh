5d450df0693a4c20cd33c236eef9a76b55cfcd58
dylan.li <dylan.li@sap.com>
2017-05-03 22:13:29 +0800

    Updated build.sh to add the command to start img server

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ docker rm -f backend
 docker rmi cndsbackend:$tag
 docker build -t cndsbackend:$tag .
 docker run -dit --name backend -v /root/img:/images -v /root/mysql/data:/var/lib/mysql -p 8080:8080 cndsbackend:$tag
+docker rm -f imgserver
+docker run -dit --name imgserver -p 8011:8080 -v /root/img:/opt/www webratio/nodejs-http-server
+
